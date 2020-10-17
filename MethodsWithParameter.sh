004f8e5724a6e759c5cdd3a80360601276daf34c
Vlad Kostyanetsky <bytecoded@gmail.com>
2020-10-17 16:51:05 +0700

    Реорганизовал скрипты

--- MethodsWithParameter.sh
+++
@@ -1,7 +0,0 @@
-# Пример поиска в выгрузке конфигурации метода (процедуры или функции), у которого есть параметр DecimalPlacesFor.
-
-# Собственно поиск.
-grep --with-filename '^[Procedure|Function].*(.*DecimalPlacesFor.*Export' */Ext/Module.bsl |
-
-# Отрезаем все лишнее, чтобы на вывод ушли только названия модулей и найденных в них методов.
-perl -pe 's/\/Ext\/Module.bsl:(Procedure|Function) /\./g;s/\(.*Export.*//g'
