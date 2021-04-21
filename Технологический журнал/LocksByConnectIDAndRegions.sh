be2ebcea4f13e5ddf13cf913ba315aa7feb4fb6c
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2021-04-21 08:55:03 +0700

    Добавил отдельный вывод Locks и alreadyLocked

--- Технологический журнал/LocksByConnectIDAndRegions.sh
+++ Технологический журнал/LocksByConnectIDAndRegions.sh
@@ -19,8 +19,8 @@
 grep --with-filename --line-number . rphost_*/*.log |
 
 # Во-первых, удалим из потока данных BOM. Во-вторых, в дальнейшем мы будем работать
-# с отдельными событиями и нам потребуется их сортировать. Переводы строк помешают
-# использовать утилиту sort, так что сразу заменим их на подстроку <AWK LB>.
+# с отдельными событиями и нам потребуется их сортировать. Во-вторых, переводы строк
+# помешают использовать утилиту sort, так что заменим их на подстроку <AWK LB>.
 #
 perl -pe 's/\xef\xbb\xbf//g; s/\r\n/<AWK LB>/g' |
 
@@ -29,19 +29,48 @@ perl -pe 's/\xef\xbb\xbf//g; s/\r\n/<AWK LB>/g' |
 # grep'ом), а потом — стандартная строка начала события в ТЖ. Перед каждым найденным
 # результатом добавим маркер начала события <AWK RS>.
 #
-# Помимо этого, вытащим из строки начала события время события и вынесем его вперед строки,
-# чтобы утилита sort смогла с ним работать.
+# Помимо этого, вытащим дату/время события и вынесем его в начало строки:
+# тогда sort сможет с этим работать.
 #
-perl -pe 's/(rphost_[0-9]+\/[0-9]+\.log:[0-9]+:([0-9]+:[0-9]+\.[0-9]+)-[0-9]+)/<AWK RS>\2 \1/g' |
+perl -pe 's/(rphost_[0-9]+\/([0-9]+)\.log:[0-9]+:([0-9]+:[0-9]+\.[0-9]+)-[0-9]+)/<AWK RS>\2 \3 \1/g' |
 
 # Разделим поток на отдельные события с помощью маркера <AWK RS> и проанализируем:
-# подходят эти события под условия из параметров скрипта или нет.
+# подходят эти события под условия из параметров скрипта или нет. Если подходят —
+# отдельными строками для каждого выведем поля Locks и alreadyLocked (если есть).
 #
 gawk -vCONNECTID=$1 -vREGIONS=$2 -vRS='<AWK RS>' '
+
+function GetFieldValue(FieldValues, BeginsWith)
+{
+    Result = "";
+    
+    BeginsWithLength = length(BeginsWith);
+    
+    for (FieldValuesKey in FieldValues) {
+           
+        Value = FieldValues[FieldValuesKey];
+           
+        ValueBeginsWith = substr(Value, 1, BeginsWithLength);
+        
+        if (ValueBeginsWith == BeginsWith) {
+            Result = Value;
+            break;
+        }
+            
+    }
+    
+    return Result;
+}
+
 {
     if ( $0 ~ ".*TLOCK,.*,t:connectID=" CONNECTID ",.*,Regions=" REGIONS ".*" ) 
     {    
-        print $0
+        split($0, FieldValues, ",");
+    
+        Locks           = GetFieldValue(FieldValues, "Locks=");
+        alreadyLocked   = GetFieldValue(FieldValues, "alreadyLocked=");        
+    
+        print $0 "\t" Locks "<AWK LB>" "\t" alreadyLocked "<AWK LB>";
     }
 }' |
 
