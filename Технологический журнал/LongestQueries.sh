9c7c505cc26f63b90579255f60f0b37b7d6932a7
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2021-04-25 17:50:27 +0700

    Загрузил новую версию

--- Технологический журнал/LongestQueries.sh
+++ Технологический журнал/LongestQueries.sh
@@ -1,9 +1,15 @@
 # Топ длительных запросов к MSSQL.
 # 
-# Запросы группируются по тексту и его контексту. Для каждого выводится общее время выполнения,
-# среднее время выполнение и количество выполнений. Сортировка — по общему времени.
+# Запросы группируются по тексту (поле Sql) и контексту (поле Context). Если текст или контекст
+# отсутствуют в записи о событии, группировка будет по тем данным, что есть — например, только
+# по запросу или только по контексту. Если нет ни запроса, ни контекста, группировка будет
+# выполнена по пустой строке.
+#
+# Для каждого выводится суммарное время выполнения, среднее и максимальное, а также общее число
+# выполнений. Сортировка — по суммарному времени, от большего к меньшему.
+#
 
-cat rphost_*/*.log |
+cat */*.log |
 
 # Удаляем из потока данных UTF-8 BOM.
 #
@@ -14,68 +20,95 @@ perl -pe 's/\xef\xbb\xbf//g' |
 #
 sed -r "/^p_[0-9]+:/d" |
 
-# Скрипт разделяет лог по метке времени события и работает только с событиями DBMSSQL.
-# Каждое событие разделяется по подстроке «Sql=»; таким образом, в первом поле у нас будет
-# строка события до текста запроса, во втором поле — Sql, Rows, RowsAffected и Context.
+# Утилита gawk разделяет лог по метке времени события и выполняет переданный ей скрипт.
 #
-# Из первого поля нам нужна только продолжительность события (т.е. время выполнения запроса).
-# Его можно получить через split(), который разделит строку по запятым. Строка до первой запятой
-# и есть длительность, так как время начала события мы уже удалили при разделении записей.
-#
-# Во втором поле нас интересует только текст запроса и контекст. Поэтому остальные два параметрам
-# мы удаляем, а также заменяем имена временных таблиц на одно универсальное (улучшит группировку)
-# и заменяем переводы строк на подстроку <LF> (если этого не сделать — сортировать будет неудобно).
-#
-# Скрипт делится на две части; первая собирает данные в массивы QueriesDuration и QueriesExecuted.
-# Первый хранит общую длительность запросов, второй — количество их выполнений. Ключи каждого — 
-# текст запроса и его контекст.
-#
-# Завершающая часть скрипта считает общую и среднюю продолжительность запросов в секундах, 
-# форматирует и выводит.
-#
-gawk -F'Sql=' -vRS='[0-9]+:[0-9]+.[0-9]+-' '
+gawk -vRS='[0-9]+:[0-9]+.[0-9]+-' '
 
-function StringBeforeComma(StringWithComma)
-{
-    return substr(StringWithComma, 0, index(StringWithComma, ",") - 1);
+function GetDuration(Input) {
+
+    # Мы разделили события по времени их начала, поэтому набор цифр до запятой — это продолжительность события.    
+    #
+    return substr(Input, 0, index(Input, ",") - 1);
+    
 }
 
-function SqlAndContext(StringAfterSql)
-{
-    gsub("\n+$", "", StringAfterSql);
-    gsub("\n", "<LF>", StringAfterSql);
-    gsub("#tt[0-9]+", "#tt", StringAfterSql);
-    gsub("Rows=[0-9]+,RowsAffected=[0-9]+,", "", StringAfterSql);
+function GetGroup(Input) {
 
-    return StringAfterSql;
+    # Определим, какие составляющие группировки есть в тексте события. Нас интересует либо Sql,
+    # либо Context, а лучше и то, и другое.
+
+    if (index(Input, "Sql=") != 0) {
+           
+        Result = Input;
+           
+        gsub("^.*Sql=", "Sql=", Result);
+        
+    }
+    else if (index(Input, "Context=") != 0) {
+                        
+        Result = Input;
+                        
+        gsub("^.*Context=", "Context=", Result);
+          
+    }
+    else {
+        
+        # Если в событии нет ни текста запроса, ни контекста выполнения —
+        # группируем по пустой строке.
+        #
+        Result = "";
+            
+    }
+    
+    if (Result != "") {
+    
+        gsub("\n+$",                                "",     Result); # удалим двойные переводы строк
+        gsub("\n",                                  "<LF>", Result); # заменим переводы строк на <LF>
+        gsub("#tt[0-9]+",                           "#tt",  Result); # нормализуем имена временных таблиц
+        gsub("Rows=[0-9]+,RowsAffected=[0-9]+,",    "",     Result); # удалим данные, не нужные для анализа    
+    
+    }
+    
+    return Result;
+
 }
 
 {
-    if ( $1 ~ "^.*,DBMSSQL.*" ) {
+    # Отбираем только события DBMSSQL.
+    #    
+    if ( $0 ~ "^[0-9]+,DBMSSQL.*" ) {
 
-        Duration = StringBeforeComma($1);
-        Grouping = SqlAndContext($2);
+        Duration    = GetDuration($0);
+        Group       = GetGroup($0);
 
-        QueriesDuration[Grouping] += Duration;
-        QueriesExecuted[Grouping] += 1;
+        QueriesDuration[Group] += Duration;
+        QueriesExecuted[Group] += 1;
+        
+        if (Duration > MaxQueriesDuration[Group]) {
+            MaxQueriesDuration[Group] = Duration;
+        }
+        
     }
 };
 
 END {
-    for (Query in QueriesDuration) {
+
+    for (Group in QueriesDuration) {
     
-        executedTotal   = QueriesExecuted[Query];
-        durationTotal   = QueriesDuration[Query] / 1000000;
-        durationAverage = QueriesDuration[Query] / QueriesExecuted[Query] / 1000000;
+        executedTotal   = QueriesExecuted[Group];
+        durationMax     = MaxQueriesDuration[Group] / 1000000;
+        durationTotal   = QueriesDuration[Group] / 1000000;
+        durationAverage = QueriesDuration[Group] / QueriesExecuted[Group] / 1000000;
         
-        printf "%.3f seconds total, %.3f seconds on average, %d executions<LF>%s<LF>\n", durationTotal, durationAverage, executedTotal, Query;
+        printf "%.3f seconds total, %.3f seconds on average, %.3f seconds at maximum, %d executions<LF>%s<LF>\n", durationTotal, durationAverage, durationMax, executedTotal, Group;
         
     }
+    
 }
 ' |
 
 sort -rn |
-head -n 100 |
+head -n 1000 |
 
 # Мы заменяли переводы строк на подстроку <LF>, чтобы результат можно было отсортировать (sort) и усечь (head).
 # Теперь можно сделать обратную замену — тогда результат будет удобнее читать.
